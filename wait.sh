8a9370015cab91ca455f7c49d3ab3af52a9c7f9d
gengrong <gr@terminus.io>
2017-04-06 17:41:30 +0800

    init

--- wait.sh
+++ wait.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 for k in $( seq 1 180 )
 do
-  nc -z 192.168.0.148 80
+  nc -z $1 $2
   if [ $? -ne 0 ]; then
-    echo "wait...$k"
+    echo "wait $1:$2...$k"
     sleep 1
   else 
     echo ok
     break
   fi
 done
-$1
+$3
